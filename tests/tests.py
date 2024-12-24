c44d0fba9bd9d1f0dbdcbc23242ae0c7679cc767
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-12-24 20:38:55 -0300

    add tests to `/md-content` and `/html-content`

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,15 @@ import unittest
 import requests
 
 
-class TestServer(unittest.TestCase):
-    def test_get_on_server_should_return_200(self):
+class TestRoutes(unittest.TestCase):
+    def test_get_on_index_should_return_200(self):
         response = requests.get("http://localhost:8080/")
+        self.assertEqual(response.status_code, 200, "Status code its not 200")
+
+    def test_get_on_html_content_should_return_200(self):
+        response = requests.get("http://localhost:8080/html-content")
+        self.assertEqual(response.status_code, 200, "Status code its not 200")
+
+    def test_get_on_md_content_should_return_200(self):
+        response = requests.get("http://localhost:8080/md-content")
         self.assertEqual(response.status_code, 200, "Status code its not 200")
