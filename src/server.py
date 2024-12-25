952e455faf69918596fd9ca8dd744f898e1fd6a2
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-12-25 14:39:01 -0300

    add markdown from file to editor

--- src/server.py
+++ src/server.py
@@ -1,8 +1,6 @@
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import markdown
-
 
 class Server:
     """
@@ -15,58 +13,28 @@ class Server:
     class RequestHandler(BaseHTTPRequestHandler):
         def do_GET(self):
             if self.path == "/" or self.path == "/index":
+                filepath = os.path.join(Server.path, Server.filename)
+
+                with open(filepath, "r", encoding="utf-8") as file:
+                    markdown_content = file.read()
+
                 with open(
-                    "/home/deividsousan/Programação/vault/src/pages/index.html",
-                    "r",
+                    file="/home/deividsousan/Programação/vault/src/pages/index.html",
+                    mode="r",
                     encoding="utf-8",
                 ) as file:
                     index_page = file.read()
 
-                    index_page = index_page.replace(
-                        "[Document]",
-                        Server.filename,
-                    )
+                    index_page = index_page.replace("[Document]", Server.filename)
 
-                    index_page = index_page.replace(
-                        "[Filename]",
-                        Server.filename,
-                    )
+                    index_page = index_page.replace("[Filename]", Server.filename)
+
+                    index_page = index_page.replace("[Markdown]", markdown_content)
 
                 self.send_response(200)
                 self.send_header("Content-Type", "text/html")
                 self.end_headers()
                 self.wfile.write(index_page.encode("utf-8"))
-
-            elif self.path == "/html-content":
-                filepath = os.path.join(Server.path, Server.filename)
-
-                if os.path.exists(filepath):
-                    self.send_response(200)
-                    self.send_header("Content-Type", "text/plain")
-                    self.end_headers()
-
-                    with open(filepath, "r", encoding="utf-8") as file:
-                        html_content = markdown.markdown(file.read())
-                        self.wfile.write(html_content.encode("utf-8"))
-                else:
-                    self.send_error(404, "Arquivo não encontrado")
-
-            elif self.path == "/md-content":
-                filepath = os.path.join(Server.path, Server.filename)
-
-                if os.path.exists(filepath):
-                    self.send_response(200)
-                    self.send_header("Content-Type", "text/plain")
-                    self.end_headers()
-
-                    with open(filepath, "r", encoding="utf-8") as file:
-                        markdown_content = file.read()
-                        self.wfile.write(markdown_content.encode("utf-8"))
-                else:
-                    self.send_error(404, "Arquivo não encontrado")
-
-            else:
-                self.send_error(404, "Endpoint não configurado")
 
     def __init__(self, path, filename):
         Server.path = path
